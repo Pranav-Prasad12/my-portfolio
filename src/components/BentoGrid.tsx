ed23258762f79532c8bfc2b0620e45c21be935d0
Pranav Prasad <pranavkarthika12@gmail.com>
2026-06-23 11:52:15 +0530

    Fix faint text color

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -13,8 +13,8 @@ export const BentoGridItem = ({ className, title, description }: { className?: s
   return (
     <div className={cn("row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 p-6 dark:bg-zinc-900 dark:border-white/[0.1] bg-white border border-neutral-200 justify-between flex flex-col space-y-4", className)}>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-200 mb-2 mt-2 text-lg">{title}</div>
-        <div className="font-sans font-normal text-neutral-400 text-sm leading-relaxed">{description}</div>
+        <div className="font-sans font-bold text-neutral-800 dark:text-neutral-200 mb-2 mt-2 text-lg">{title}</div>
+        <div className="font-sans font-normal text-neutral-600 dark:text-neutral-400 text-sm leading-relaxed">{description}</div>
       </div>
     </div>
   );
